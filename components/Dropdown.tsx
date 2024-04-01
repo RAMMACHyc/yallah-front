a733248a59ba297a8f2f7a09e11c8ce9c900baa0
RAMMACHyc <sadik.ram12@gmail.com>
2024-04-01 11:06:06 +0000

    Refactor Dropdown component to include cities from API

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,67 +1,63 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
 import { Dropdown } from 'react-native-element-dropdown';
 import { AntDesign } from '@expo/vector-icons';
-import category from '@/utils/category.json'
-
+import { useAppSelector, useAppDispatch } from '@/store';
+import { getCities } from '@/features/city/cityThunks';
+import { CityResponse } from '@/types/city';
 
 interface Item {
   label: string;
   value: string;
 }
 
-const data: Item[] = [
-  { label: 'Item 1', value: '1' },
-  { label: 'Item 2', value: '2' },
-  { label: 'Item 3', value: '3' },
-  { label: 'Item 4', value: '4' },
-  { label: 'Item 5', value: '5' },
-  { label: 'Item 6', value: '6' },
-  { label: 'Item 7', value: '7' },
-  { label: 'Item 8', value: '8' },
-];
-
 const DropdownComponent: React.FC = () => {
   const [value, setValue] = useState<string | null>(null);
+  const cities = useAppSelector((state: { cities: CityResponse }) => state.cities.cities);
+  const dispatch = useAppDispatch();
 
-  const renderItem = (item: Item) => {
-    return (
-      <View style={styles.item}>
-        <Text style={styles.textItem}>{item.label}</Text>
-        {item.value === value && (
-          <AntDesign style={styles.icon} color="black" name="find" size={20} />
-        )}
-      </View>
-    );
-  };
+  useEffect(() => {
+    dispatch(getCities());
+  }, []);
 
-  return (
-    <Dropdown
-      style={styles.dropdown}
-      placeholderStyle={styles.placeholderStyle}
-      selectedTextStyle={styles.selectedTextStyle}
-      inputSearchStyle={styles.inputSearchStyle}
-      iconStyle={styles.iconStyle}
-      data={data}
-      search
-      maxHeight={300}
-      labelField="label"
-      valueField="value"
-      placeholder="Country"
-      searchPlaceholder="Search..."
-      value={value}
-      onChange={(item: Item | null) => {
-        if (item) {
-            setValue(item.value);
-        } else {
-            setValue(null);
-        }
-    }}
-      renderLeftIcon={() => (
+  const renderItem = (item: { label: string; value: string; }, selected?: boolean | undefined) => (
+    <View style={styles.item}>
+      <Text style={styles.textItem}>{item.label}</Text>
+      {selected && (
         <AntDesign style={styles.icon} color="black" name="find" size={20} />
       )}
-      renderItem={renderItem}
-    />
+    </View>
+  );
+  
+
+  return (
+<Dropdown
+  style={styles.dropdown}
+  placeholderStyle={styles.placeholderStyle}
+  selectedTextStyle={styles.selectedTextStyle}
+  inputSearchStyle={styles.inputSearchStyle}
+  iconStyle={styles.iconStyle}
+  
+  data={cities?.cities?.map((city) => ({ label: city.name, value: city.id.toString() })) || []}
+  search
+  maxHeight={300}
+  labelField="label"
+  valueField="value"
+  placeholder="City"
+  searchPlaceholder="Search..."
+  value={value}
+  onChange={(item: Item | null) => {
+    if (item) {
+      setValue(item.value);
+    } else {
+      setValue(null);
+    }
+  }}
+  renderLeftIcon={() => (
+    <AntDesign style={styles.icon} color="black" name="find" size={20} />
+  )}
+  renderItem={renderItem}
+/>
   );
 };
 
@@ -69,45 +65,22 @@ export default DropdownComponent;
 
 const styles = StyleSheet.create({
   dropdown: {
-   width: 130,
+    width: 140,
     height: 40,
     backgroundColor: 'white',
     borderRadius: 20,
     padding: 14,
-    // shadowColor: '#000',
-    // shadowOffset: {
-    //   width: 0,
-    //   height: 1,
-    // },
-    // shadowOpacity: 0.2,
-    // shadowRadius: 1.41,
-    // elevation: 2,
   },
-  icon: {
-    marginRight: 5,
-  },
+  icon: { marginRight: 5 },
   item: {
     padding: 17,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
   },
-  textItem: {
-    flex: 1,
-    fontSize: 16,
-  },
-  placeholderStyle: {
-    fontSize: 16,
-  },
-  selectedTextStyle: {
-    fontSize: 16,
-  },
-  iconStyle: {
-    width: 20,
-    height: 20,
-  },
-  inputSearchStyle: {
-    height: 40,
-    fontSize: 16,
-  },
+  textItem: { flex: 1, fontSize: 16 },
+  placeholderStyle: { fontSize: 16 },
+  selectedTextStyle: { fontSize: 16 },
+  iconStyle: { width: 20, height: 20 },
+  inputSearchStyle: { height: 40, fontSize: 16 },
 });
