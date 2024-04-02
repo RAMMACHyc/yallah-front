e2d22086419b45dd5cbda9ddaac9ac30af07372d
RAMMACHyc <sadik.ram12@gmail.com>
2024-04-02 22:23:09 +0000

    Refactor PlaceType interface and add LocationData interface

--- types/place.ts
+++ types/place.ts
@@ -1,9 +1,26 @@
 export interface PlaceType {
     id?: number;
     city: string;
+    placeName: string;
     categoryId: number;
-    latutude: number;
+    category: {
+        IconName: string;
+        color: string;
+        name: string;
+    };
+    latitude: number;
     longitude: number;
   }
   
  
+  export interface LocationData  {
+    category: {
+        IconName: string;
+        color: string;
+        name: string;
+    };
+    city: string;
+    placeName: string;
+    latitude: number;
+    longitude: number;
+};
